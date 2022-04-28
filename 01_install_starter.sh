735bf474613d7ad48b9f92eedcbb3a0151be9a17
Loiseau2nuit <l.oiseau2nuit@gmail.com>
2022-04-28 19:10:27 +0200

    Re-upload 2/3

---
+++ 01_install_starter.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# some usefull packages when it comes to dealing with a debien server
+#
+if [ "$(whoami)" != "root" ]; then
+    SUDO=sudo
+fi
+
+# some might already be installed, depending on your distribution
+${SUDO} apt -y install apticron ccze cowsay curl dnsutils exa fortunes git htop neofetch net-tools rsync sudo tmux whois zsh
+
+# set ZSH up as your default shell
+${SUDO} chsh -s $(which zsh)
+
+# Install OhMyZsh and follow steps (you'll thank me later !)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# add some new lines to ~/.zshrc (this part actually needs to be tested)
+echo " "
+echo " \# some new adds of mine " >> ~/.zshrc
+echo " alias ls='ls -ailsh --color' " >> ~/.zshrc
+echo " alias lx='exa -lRTL=2' " >> ~/.zshrc
+echo " alias top='htop' " >> ~/.zshrc
+echo " alias rm='rm -i' " >> ~/.zshrc
+echo " alias mv='mv -i' " >> ~/.zshrc
+echo " alias cp='cp -i' " >> ~/.zshrc
+echo "  " >> ~/.zshrc
+echo " cd ~ " >> ~/.zshrc
+echo " clear " >> ~/.zshrc
+echo " neofetch " >> ~/.zshrc
+echo " echo \"=========================================================\" " >> ~/.zshrc
+echo " echo \"Checking for upgrades ...\" " >> ~/.zshrc
+echo " apt list --upgradable " >> ~/.zshrc
+echo " echo \"run 'sudo apt update && sudo apt full-upgrade -y' if any.\" " >> ~/.zshrc
+echo " echo \"=========================================================\" " >> ~/.zshrc
+echo "  " >> ~/.zshrc
+echo " chuck_cow " >> ~/.zshrc
+echo " echo \"=========================================================\" " >> ~/.zshrc
