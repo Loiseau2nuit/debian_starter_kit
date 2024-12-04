566feb64a77ddedd0ef4d0e62ad373da22cd3158
Loiseau2nuit <l.oiseau2nuit@gmail.com>
2024-12-04 15:19:36 +0100

    [fix] going back on 0a600e1b00190bddb16f45ed3ede0afa448a6ffc & re-adding ccze "because discontinued doesn't mean useless" + [add] iptables, iptables-persistent & rkhunter

--- 01_install_starter.sh
+++ 01_install_starter.sh
@@ -9,7 +9,7 @@ fi
 ${SUDO} timedatectl set-timezone "Europe/Paris"
 
 # some might already be installed, depending on your distribution
-${SUDO} apt-get -y install apticron cowsay curl dnsutils exa fail2ban fortunes bpytop netcat-openbsd net-tools parted python3-pygments rsync screen sudo tmux unzip whois zip zsh
+${SUDO} apt-get -y install apticron bpytop ccze cowsay curl dnsutils exa fail2ban fortunes iptables iptables-persistent netcat-openbsd net-tools parted python3-pygments rkhunter rsync screen sudo tmux unzip whois zip zsh
 
 # glpi-agent
 cd
